84cb54298f4aeb6618ab85c5794cd97c8f3316ca
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-11-04 21:39:04 -0400

    react-hooks-state-and-events-codealong/
    react-hooks-state-and-events-codealong/

---
+++ Toggle.js
@@ -0,0 +1,15 @@
+function Toggle() {
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+}
